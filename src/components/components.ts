a6bb3df22250c64bfbb51b0ca4888772e8bddcc3
Sergio Lindau <38226617+sergiolindau@users.noreply.github.com>
2026-08-09 13:00:06 -0300

    Dependecies updated (`mathjslab` 2.5.0). Update in version 0.0.8 on Sun, 09 Aug 2026 16:00:01 GMT

--- src/components/components.ts
+++ src/components/components.ts
@@ -1,4 +1,4 @@
-/* Web components. File generated at Tue, 04 Aug 2026 01:19:50 GMT. */
+/* Web components. File generated at Sun, 09 Aug 2026 15:59:37 GMT. */
 export * from './batch-code-editor/batch-code-editor.component';
 export * from './batch-output/batch-output.component';
 export * from './batch-shell/batch-shell.component';
