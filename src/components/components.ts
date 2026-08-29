ee7e4ae37c52361603266b5ac245cbccb294cf6f
Sergio Lindau <38226617+sergiolindau@users.noreply.github.com>
2026-08-29 01:23:34 -0300

    Prepare 0.1.0 multilingual endpoint release Update in version 0.1.0 on Sat, 29 Aug 2026 04:23:29 GMT

--- src/components/components.ts
+++ src/components/components.ts
@@ -1,4 +1,4 @@
-/* Web components. File generated at Sat, 29 Aug 2026 01:34:16 GMT. */
+/* Web components. File generated at Sat, 29 Aug 2026 04:22:41 GMT. */
 export * from './batch-code-editor/batch-code-editor.component';
 export * from './batch-output/batch-output.component';
 export * from './batch-shell/batch-shell.component';
